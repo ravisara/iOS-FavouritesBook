5d6436039bc826f1c1759ea25f39926a7013440a
Ravisara S. Kariyawasam <ravisara@gmail.com>
2021-05-08 16:19:03 +0100

    Initial Commit

---
+++ FavouritesBook/FavouritesBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritesBookApp.swift
+//  FavouritesBook
+//
+//  Created by StarChanger on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FavouritesBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
